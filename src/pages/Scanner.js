360f24deb36d2ce66cba46a4faf20554b2e69c2d
varunvaatsalya <a.varungupta23@gmail.com>
2024-05-28 15:40:23 +0530

    scanner page

--- src/pages/Scanner.js
+++ src/pages/Scanner.js
@@ -5,6 +5,8 @@ function Scanner() {
   return (
     <>
       <Navbar/>
+      <div className="font-bold text-3xl text-center my-2">Scanner Page</div>
+      <div className="font-bold text-purple-400 text-3xl text-center my-2">Coming Soon</div>
     </>
   )
 }
